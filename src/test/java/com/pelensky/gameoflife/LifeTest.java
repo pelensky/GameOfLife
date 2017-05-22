2203c2acd41b5a64be9eafe4c5ec9856cc5fff8d
Dan Pelensky <dan@pelensky.com>
2017-05-22 16:36:10 +0100

    Testing every part of next generation

--- src/test/java/com/pelensky/gameoflife/LifeTest.java
+++ src/test/java/com/pelensky/gameoflife/LifeTest.java
@@ -99,8 +99,34 @@ public class LifeTest {
     public void nextGeneration() {
         life.nextGeneration();
         assertFalse(life.getGrid().get(0).get(0).isAlive());
+        assertFalse(life.getGrid().get(0).get(1).isAlive());
+        assertFalse(life.getGrid().get(0).get(2).isAlive());
+        assertFalse(life.getGrid().get(0).get(3).isAlive());
+        assertFalse(life.getGrid().get(0).get(4).isAlive());
+
+        assertFalse(life.getGrid().get(1).get(0).isAlive());
+        assertFalse(life.getGrid().get(1).get(1).isAlive());
         assertTrue(life.getGrid().get(1).get(2).isAlive());
+        assertFalse(life.getGrid().get(1).get(3).isAlive());
+        assertFalse(life.getGrid().get(1).get(4).isAlive());
+
+        assertFalse(life.getGrid().get(2).get(0).isAlive());
+        assertTrue(life.getGrid().get(2).get(1).isAlive());
+        assertFalse(life.getGrid().get(2).get(2).isAlive());
+        assertTrue(life.getGrid().get(2).get(3).isAlive());
+        assertFalse(life.getGrid().get(2).get(4).isAlive());
+
+        assertFalse(life.getGrid().get(3).get(0).isAlive());
+        assertTrue(life.getGrid().get(3).get(1).isAlive());
+        assertFalse(life.getGrid().get(3).get(2).isAlive());
+        assertTrue(life.getGrid().get(3).get(3).isAlive());
+        assertTrue(life.getGrid().get(3).get(4).isAlive());
+
+        assertFalse(life.getGrid().get(4).get(0).isAlive());
+        assertFalse(life.getGrid().get(4).get(1).isAlive());
         assertFalse(life.getGrid().get(4).get(2).isAlive());
+        assertTrue(life.getGrid().get(4).get(3).isAlive());
+        assertFalse(life.getGrid().get(4).get(4).isAlive());
     }
 
 }
