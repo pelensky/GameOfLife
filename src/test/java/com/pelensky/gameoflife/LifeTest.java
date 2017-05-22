3582505f49b0ceffd0c0f934ebab4754b8866225
Dan Pelensky <dan@pelensky.com>
2017-05-22 14:02:24 +0100

    Removed FakeGrid object - initializing it inside test

--- src/test/java/com/pelensky/gameoflife/LifeTest.java
+++ src/test/java/com/pelensky/gameoflife/LifeTest.java
@@ -4,6 +4,7 @@ package com.pelensky.gameoflife;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -12,11 +13,70 @@ public class LifeTest {
 
     private Life life;
     private List<List<Cell>> grid;
+    private final Cell x0y0 = new NotLivingCell();
+
+    private void createFakeGrid() {
+        grid = new ArrayList<>();
+        grid.add(row0());
+        grid.add(row1());
+        grid.add(row2());
+        grid.add(row3());
+        grid.add(row4());
+    }
+
+    private List<Cell> row0() {
+        List<Cell> row = new ArrayList<>();
+        row.add(x0y0);
+        row.add(new LivingCell());
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        return row;
+    }
+
+    private List<Cell> row1() {
+        List<Cell> row = new ArrayList<>();
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        return row;
+    }
+
+    private List<Cell> row2() {
+        List<Cell> row = new ArrayList<>();
+        row.add(new NotLivingCell());
+        row.add(new LivingCell());
+        row.add(new NotLivingCell());
+        row.add(new LivingCell());
+        row.add(new NotLivingCell());
+        return row;
+    }
+
+    private List<Cell> row3() {
+        List<Cell> row = new ArrayList<>();
+        row.add(new NotLivingCell());
+        row.add(new LivingCell());
+        row.add(new LivingCell());
+        row.add(new LivingCell());
+        row.add(new NotLivingCell());
+        return row;
+    }
+
+    private List<Cell> row4() {
+        List<Cell> row = new ArrayList<>();
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        row.add(new NotLivingCell());
+        row.add(new LivingCell());
+        row.add(new NotLivingCell());
+        return row;
+    }
 
     @Before
     public void setUp(){
-        FakeGrid fakeGrid = new FakeGrid();
-        grid = fakeGrid.fakeGrid();
+        createFakeGrid();
         life = new Life(grid);
     }
 
